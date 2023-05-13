0c4152041122b336be9e0fc0db4064c79f0c267a
abooBala <aboobala64@gmail.com>
2023-05-13 12:00:08 +0100

    Registration page

--- dcrm/app/views.py
+++ dcrm/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
+from .forms import SignUpForm
 
 def home(request):
     # check to see if the user is logged in 
@@ -25,3 +26,19 @@ def logout_user(request):
     messages.success(request, "You have been logged out.")
     return redirect('home')
 
+def register_user(request):
+    if request.method == 'GET':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered")
+            return redirect('home')
+        
+        else: 
+            form = SignUpForm()
+            return render(request, 'register.html', {'form':form})
